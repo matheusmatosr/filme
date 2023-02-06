d76881c134d144c321a48ba5b189c82e751ef643
Matheus Matos <100291312+matheusmatosr@users.noreply.github.com>
2023-02-06 11:54:41 -0300

    update
    update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <header>
-            <Link className='logo' to="/">Matos Flix</Link>
+            <Link className='logo' to="/">MatosFlix</Link>
             <Link className='favoritos' to="/favoritos">Meus filmes</Link>
         </header>
     )
